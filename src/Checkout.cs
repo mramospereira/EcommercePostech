e14b4ebcca7a291a3db477ee85b24ed7cf090a90
Mateus Ramos Pereira <mateusramospereira@hotmail.com>
2024-01-05 17:56:26 -0300

    Teste build

--- src/Checkout.cs
+++ src/Checkout.cs
@@ -22,7 +22,7 @@ namespace MyEcommerce.Checkout
             var response = req.CreateResponse(HttpStatusCode.OK);
             response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
 
-            response.WriteString("Welcome to Azure Functions!");
+            response.WriteString("Welcome to Azure Functions! Deploy test");
 
             return response;
         }
